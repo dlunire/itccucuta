80107cc71d7577a1cd5f5645f7c5a33d658794b5
Códigos del Futuro <dlunireframework@gmail.com>
2025-05-21 03:44:25 -0500

    Update

--- frontend/src/layouts/components/Notifications/NotificationFile.svelte
+++ frontend/src/layouts/components/Notifications/NotificationFile.svelte
@@ -1,25 +1,42 @@
 <script lang="ts">
     import { onMount } from "svelte";
+    import ButtonClose from "../Buttons/ButtonClose.svelte";
 
     export let content: Function | undefined = undefined;
+    export let time: number = 3000;
+    export let open: boolean = true;
 
     let notification: HTMLElement | null = null;
-
+    let timeout: number | null = null;
     onMount(() => {
         if (!(notification instanceof HTMLElement)) return;
         document.body.appendChild(notification);
+
+        timeout = setTimeout(() => {}, time);
     });
+
+    function onclick() {
+        open = false;
+    }
 </script>
 
-<section class="notification notification--file" bind:this={notification}>
-    <div class="notification__inner notification__inner--info">
-        {#if content}
-            {@render content()}
-        {:else}
-            <span
-                >Agregue su mensaje aquí Lorem ipsum dolor sit amet consectetur
-                adipisicing elit. Ad, ducimus.</span
-            >
-        {/if}
-    </div>
-</section>
+{#if open}
+    <section class="notification notification--file" bind:this={notification}>
+        <div class="notification__inner notification__inner--info">
+            {#if content}
+                {@render content()}
+            {:else}
+                <span
+                    >Agregue su mensaje aquí Lorem ipsum dolor sit amet
+                    consectetur adipisicing elit. Ad, ducimus.</span
+                >
+            {/if}
+
+            <ButtonClose {onclick}>
+                {#snippet content()}
+                    <span>Cerrar</span>
+                {/snippet}
+            </ButtonClose>
+        </div>
+    </section>
+{/if}
