9fee6bb7776bd6de8ec69aa17e2a4b95bc2751b8
Códigos del Futuro <dlunireframework@gmail.com>
2025-07-10 14:25:27 -0500

    Update

--- frontend/src/layouts/components/Nav/Buttons.svelte
+++ frontend/src/layouts/components/Nav/Buttons.svelte
@@ -1,14 +1,36 @@
 <script lang="ts">
+    import IconCsv from "../../icons/IconCSV.svelte";
     import IconHelp from "../../icons/IconHelp.svelte";
+    import IconRegister from "../../icons/IconRegister.svelte";
     import IconSettings from "../../icons/IconSettings.svelte";
 </script>
 
 <nav class="nav">
-    <button class="button button--settings" aria-label="Configuración">
+    <button
+        class="button button--nav"
+        aria-label="Agregar archivo CSV"
+        title="Agregar archivo CSV"
+    >
+        <IconCsv />
+        <span>Archivo CSV</span>
+    </button>
+    <button
+        class="button button--nav"
+        aria-label="Editar registro"
+        title="Editar registro"
+    >
+        <IconRegister />
+        <span>Editar registro</span>
+    </button>
+    <button
+        class="button button--nav"
+        aria-label="Configuración"
+        title="Configuración"
+    >
         <IconSettings />
         <span>Configuración</span>
     </button>
-    <button class="button button--help">
+    <button class="button button--nav" title="Ayuda">
         <IconHelp />
         <span>Ayuda</span>
     </button>
