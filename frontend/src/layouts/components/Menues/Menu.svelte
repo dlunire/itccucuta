83ec594ac4834c7b85ce82009b6f2fe3c2ebfd16
Códigos del Futuro <dlunireframework@gmail.com>
2025-07-10 11:21:31 -0500

    Update

--- frontend/src/layouts/components/Menues/Menu.svelte
+++ frontend/src/layouts/components/Menues/Menu.svelte
@@ -1,13 +1,35 @@
-<script>
+<script lang="ts">
+    import type { SvelteComponent } from "svelte";
     import MenuItem from "./MenuItem.svelte";
+    import IconCsv from "../../icons/IconCSV.svelte";
+    import IconRegister from "../../icons/IconRegister.svelte";
+    import IconTime from "../../icons/IconTime.svelte";
+    import IconSearchRegister from "../../icons/IconSearchRegister.svelte";
+
+    let iconCSV: typeof SvelteComponent = IconCsv as typeof SvelteComponent;
+    let iconRegister: typeof SvelteComponent =
+        IconRegister as typeof SvelteComponent;
+
+    let iconTime: typeof SvelteComponent = IconTime as typeof SvelteComponent;
+    let iconSearchRegister: typeof SvelteComponent =
+        IconSearchRegister as typeof SvelteComponent;
 </script>
 
 <ul class="menu">
-    <MenuItem href="/dashboard" label="Carga de archivo CSV" />
+    <MenuItem href="/dashboard" label="Archivos CSV" icon={iconCSV} />
     <MenuItem
         href="/dashboard/debbug-register"
         label="Corrección de registros"
+        icon={iconRegister}
     />
-    <MenuItem href="/dashboard/history" label="Historial de carga" />
-    <MenuItem href="/dashboard/register" label="Buscador de registro" />
+    <MenuItem
+        href="/dashboard/history"
+        label="Historial de carga"
+        icon={iconTime}
+    />
+    <MenuItem
+        href="/dashboard/register"
+        label="Buscador de registro"
+        icon={iconSearchRegister}
+    />
 </ul>
