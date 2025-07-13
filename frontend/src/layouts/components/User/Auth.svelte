c34d8976c8cbda935812c95c305060b546719a09
Códigos del Futuro <dlunireframework@gmail.com>
2025-07-13 13:51:30 -0500

    Update

--- frontend/src/layouts/components/User/Auth.svelte
+++ frontend/src/layouts/components/User/Auth.svelte
@@ -23,12 +23,34 @@
         if (!(header instanceof HTMLElement)) return;
 
         const size: DOMRect = header.getBoundingClientRect();
-        top = size.height + 10;
+        top = size.height + 5;
     }
+
+    addEventListener("click", function (event: MouseEvent) {
+        const { target: element } = event;
+        if (!(element instanceof HTMLElement)) return;
+
+        const { open } = element.dataset;
+        if (open || open === "false") return;
+        openMenu = false;
+    });
+
+    addEventListener("keydown", function (event: KeyboardEvent) {
+        const { key } = event;
+        if (key === "Escape") {
+            openMenu = false;
+        }
+    });
 </script>
 
 <nav class="auth">
-    <button class="button button--user" aria-label="Usuario" {title} {onclick}>
+    <button
+        class="button button--user"
+        aria-label="Usuario"
+        {title}
+        {onclick}
+        data-open={String(openMenu)}
+    >
         <IconUser />
     </button>
 </nav>
